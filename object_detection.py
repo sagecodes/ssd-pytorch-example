7e5b077638faed29999ac805afddb34e2d7fcca6
Sage Elliott <hello@sageelliott.com>
2018-10-09 18:17:27 -0700

    Tranfrm frams for NN

--- object_detection.py
+++ object_detection.py
@@ -4,3 +4,9 @@ import cv2
 from data import BaseTransform,VOC_CLASSES as labelmap
 from ssd import build_ssd
 import imageio
+
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    frame_t = transform(frame)[0]
+    x = torch.from_numpy(frame_t).permute(2, 0, 1)
+    x = Variable(x.unsqueeze(0))
